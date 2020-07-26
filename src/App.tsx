7586241300d1e4126488c6018d937222e7841809
Jerry Thimothy J <jerrythimothy@gmail.com>
2020-07-26 20:30:45 +0400

    Setup loader #9

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import Loader from "react-loader"
 import logo from './logo.svg';
 import './App.scss';
 import { fetchGithubUsers } from './services/github/github.service';
+import { showLoaderCountState } from './services/api/api.service';
 
 function App() {
 
+  const [isShowLoader, setIsShowLoader] = useState(0)
+
   useEffect(() => {
-    fetchGithubUsers("jerry", "users", 1, 100)
+    showLoaderCountState.subscribe((showLoaderCount: any) => {
+      setIsShowLoader(showLoaderCount)
+    })
+    fetchGithubUsers("jerrythi", "users", 1, 100)
   }, [])
 
   return (
     <div className="App">
+      <Loader loaded={!isShowLoader}></Loader>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
--- src/services/api/api.service.ts
+++ src/services/api/api.service.ts
@@ -1,8 +1,36 @@
 import axios from "axios";
+import { Subject } from "rxjs"
 
 const instance = axios.create({
     baseURL: "http://localhost:4100/api/"
 });
+let showLoaderCount = 0;
+const showLoaderCountSubject = new Subject();
+export const showLoaderCountState = showLoaderCountSubject.asObservable()
+
+instance.interceptors.request.use(function (config) {
+    showLoaderCount++;
+    showLoaderCountSubject.next(showLoaderCount)
+
+    return config
+}, function (error) {
+    showLoaderCount--;
+    showLoaderCountSubject.next(showLoaderCount)
+
+    return Promise.reject(error)
+})
+
+instance.interceptors.response.use(function (config) {
+    showLoaderCount--;
+    showLoaderCountSubject.next(showLoaderCount)
+
+    return config
+}, function (error) {
+    showLoaderCount--;
+    showLoaderCountSubject.next(showLoaderCount)
+
+    return Promise.reject(error)
+})
 
 export const ajaxPost = (url: string, data: any) => instance({
     method: 'post',
