ae0db9da86feb6ff41ebd89265eee4854380a37f
Jerry Thimothy J <jerrythimothy@gmail.com>
2020-07-27 23:30:08 +0400

    User should be able to search using an entity #10
    * Setup Router - React Router #2; Setup data / state layer - Redux #3
    
    * Setup Router - React Router #2; Setup data / state layer - Redux #3
    
    * User should be able to search using an entity #10
    
    * Removed unncessary codes

--- src/containers/github/github.container.tsx
+++ src/containers/github/github.container.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import ReactDOM from 'react-dom';
 import { useSelector, useDispatch } from 'react-redux'
 import { getData } from '../../actions/github/github.action'
@@ -6,15 +6,25 @@ import _ from "lodash";
 
 export const Github = () => {
 
+    const searchCriteria = useSelector((state: any) => state.githubReducer.searchCriteria)
     const usersData = useSelector((state: any) => state.githubReducer.data)
     const dispatch = useDispatch()
 
     useEffect(() => {
-        dispatch(getData({ text: "jer", type: "users", page: 1, per_page: 100 }))
+        dispatch(getData(searchCriteria))
     }, [])
+
+    const handleEntityChange = useCallback((event: any) => {
+        dispatch(getData({ ...searchCriteria, type: event.target.value }))
+    }, [searchCriteria])
 
     return (
         <div>
+            <select onChange={handleEntityChange}>
+                <option value="users">Users</option>
+                <option value="repositories">Repositories</option>
+                <option value="issues">Issues</option>
+            </select>
             {_.map(usersData && usersData.items, (item, index) => {
                 return <div key={index}>{item.login}</div>
             })}
